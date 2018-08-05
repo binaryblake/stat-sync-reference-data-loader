d39af16ac2f5911fa1b369672e80d1b38bdcfd17
BLAKE JANELLE <BLAKE.JANELLE@LIBERTYMUTUAL.COM>
2018-08-05 15:51:28 -0400

    initial upload for reference data loader, which will import player, team, and season data

---
+++ src/main/java/io/statsync/referencedataload/ReferenceDataLoadApplication.java
@@ -0,0 +1,12 @@
+package io.statsync.referencedataload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceDataLoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceDataLoadApplication.class, args);
+	}
+}
